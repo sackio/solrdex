2c9605bacaf2abab008674c5f09528354dcd826e
Ben Sack <ben@sack.io>
2014-09-21 17:18:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# solrdex [![Build Status](https://secure.travis-ci.org/ben/solrdex.png?branch=master)](http://travis-ci.org/ben/solrdex)
+
+Use Solr as a secondary index and search engine
+
+## Getting Started
+Install the module with: `npm install solrdex`
+
+```javascript
+var solrdex = require('solrdex');
+solrdex.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Ben Sack  
+Licensed under the MIT license.
---
+++ test/solrdex_test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var solrdex = require('../lib/solrdex.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['tests'] = {
+  setUp: function(done) {
+    done();
+  },
+  'solr': function(test) {
+    test.ok(true);
+    test.done();
+  },
+};
